533266ca8dcdff78f4e7d7d0b64f523d436ab900
Matthew Clemens <mclemens66+github@gmail.com>
2011-09-05 08:10:49 -0700

    added 2nd display option

--- cprog/lab2.c
+++ cprog/lab2.c
@@ -12,6 +12,7 @@ int main(void)
 {
   int product_number, product_type, product_quantity;  
   double cost, price;
+  char choice;
     
     printf("%s\n\n", NAME);
 
@@ -30,11 +31,31 @@ int main(void)
     printf("Enter the price:");
     scanf("%lf%*c", &price);
 
-    printf("The product number is %d\n", product_number);
-    printf("The product type is %d\n", product_type);
-    printf("The quanity is %d\n", product_quantity);
-    printf("The cost is %.2lf\n", cost);
-    printf("The price is %.2lf\n", price);
+    printf("\nThe product number is ------ %d\n", product_number);
+    printf("The product type is ------ %d\n", product_type);
+    printf("The quanity is ------ %d\n", product_quantity);
+    printf("The cost is ------ $%.2lf\n", cost);
+    printf("The price is ------ $%.2lf\n", price);
 
+    printf("Would you like to see and alternate view? (type y/n)\n");
+
+    while(scanf(" %c%*c", &choice)==1){
+       
+        if (choice=='y' || choice =='Y') 
+        {   printf("%20s %20s %20s %10s %10s\n", "Product number", 
+                "Product type", "product quantity", "cost", "price");
+            printf("%20d %20d %20d %10.2lf %10.2lf\n", product_number, 
+                    product_type, product_quantity, cost, price);
+            break;
+        }
+    
+        if(choice=='n'){
+            printf("Ok then, BYE!\n");
+            break;
+        } 
+    }
+        
+
+return 0;
 } 
 
