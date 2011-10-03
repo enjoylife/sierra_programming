0e6705aac89b80b16e29d7dbb579536c4537d3d1
Matthew Clemens <matthew@mpc.(none)>
2011-10-02 21:54:58 -0700

    Added to the ChangeBack class, by changing logic to use modulo function. However it has yet to be tested.
    Also started to work on the TriShape class, but needs to add control
    flow for if it is a triangle.

--- javaprog/mattC_codewalk.java
+++ javaprog/mattC_codewalk.java
@@ -53,7 +53,7 @@ class CodeWalk {
      * return the amount back as a string of quarters,
      * dimes, nickels, and pennies, it takes.
      * CodeWalk c = new CodeWalk();
-     * c.ChangeBack(1.0) -> " Your change for $1.00 is  4 quarters."
+     * c.ChangeBack(1.0) -> " Your change for $1.00 is  4 quarters, 0 dimes, 0 nickels, and 0 pennies."
      * c.ChangeBack(2.17) -> " Your change for $2.17 is 8 quarters, 1 dimes, 1 nickels, and 2 pennies."
      * c.ChangeBack(.37) -> "Your change for $0.37 is 3 dimes, 1 nickels, and 2 pennies."
      */
@@ -63,15 +63,41 @@ class CodeWalk {
         if(money<=0){
             return "No change back for you!";
         }
-        else if(money%1==0)  {
-            return "Your change for $ "+ money+" is " +
+        else   {
+         quarters= money%4;
+         dimes = quarters%.10;
+         nickels = dimes %.5;
+         pennies = nickels%.1;
 
-            
-         quarters= money/4;
-         dimes = quarters/.10;
-         nickels = dimes /.5;
-         pennies = nickels/.1;
-         return "Your change for $" + money + " is " + quarters +" quarters, " + dimes +" dimes, "+ nickels+" nickels, "+pennies+" pennies.";
+                    return "Your change for $" + money + " is " + quarters +" quarters, " + dimes +" dimes, "+ nickels+" nickels, "+pennies+" pennies.";
         }
+    }
+
+    /**
+     * TriShape: side1 (int) side2 (int) side3(int) -> String
+     * Given the lengths of the sides of a triangle, return the strings , 
+     * "equilateral", "isoscoles", "scalene",or "not a triangle" depending upon the mathematical 
+     * definitions of side length for each.
+     * CodeWalk c = new CodeWalk();
+     * c.TriShape(2,2,2) -> "equilateral"
+     * c.TriShape(3,4,5) -> "scalene"
+     * c.TriShape(1,2,7) -> "not a triangle"
+     * c.TriShape(6,6,9) -> "isoscoles"
+     * c.TriShape(0,0,0) -> "not a triangle"
+     */
+    String TriShape(int side1, int side2, int side3){
+        boolean test1, test2, test3;
+
+        test1=(side1+side2)>side3;
+        test2=(side1+side3)>side2;
+        test3=(side2+side3)>side1;
+
+        if(!(test1 &&test2 &&test3)){
+        return "not a triangle";
+        }
+        else{ 
+            test1=(side1==side2 && side1==side3);
+            return "";
+        } 
     }
 }
