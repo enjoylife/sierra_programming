7c0e60be1a94d1ebece0dbd0f4231e34514f5669
Matthew Clemens <mclemens66+github@gmail.com>
2011-10-04 10:33:41 -0700

    fixed menu do while looop
    need to add to the menu functionality.
    Need to add the report, change, and delete functions.
    Eventually need to create a more permeant storage technique for the
    data.

--- cprog/lab5.c
+++ cprog/lab5.c
@@ -6,6 +6,7 @@ Author = Matthew Clemens
 Date = 10/03/11
 --------------------------------------------*/
 #include <stdio.h>
+#include <stdbool.h>
 #define NAME "Sierra Sporting Goods"
 
 double Profit(double, double);
@@ -17,77 +18,38 @@ void ValidateDouble(double * check, double a, double b, char[]);
 int menu(void);
 void add(void);
 void show(int, int, int, double, double);
+void showTotal(int, double, double, double);
 
 int main(void)
 {
     
     int choice;
+    bool more=true;
+
     do{
     choice = menu(); 
-    add();
-   printf("Totals of all products\n");
-   showTotals();
-   
-    }while(scanf("%d%*c", &choice)==5)
-   sum_quantity+=1;
+    switch(choice)
+    {
+        case 1: add();
+                break;
+        case 2:
+        case 3:
+        case 4:
+        case 5: more=false;
+                break;
+        default: printf("GoodBye\n");
+                 more =false;
+                 break;
+    }
 
-   /* Summations of Products */
-   printf("Total count of all products -> %d\n", sum_quantity);
-   printf("Total of all prices ------ %4.2lf\n", sum_price);
-   printf("Total of all costs ------ $%4.2lf\n", sum_cost);
-   printf("Total profit ------ $ %4.2lf\n", sum_profit);
-        
+   
+    }while(more);
+
+       
 
 return 0;
 } 
 
-double Profit(double tp, double tc){
-    double total;
-    total = tp - tc;
-    return total;
-}
-
-double Price(int q , double p){
-    double total;
-    total = q * p;
-    return total;
-}
-
-double Cost(double c, int q){
-    double total;
-    total = c * q;
-    return total;
-}
-void ValidateInt(int * check, int a, int b, char []title){
-    printf("Enter the %s\n", title);
-    while(scanf("%d%*c", check)==1){
-       
-        if (*check > b|| *check < a) 
-        {
-            printf("Invalid entry.\nTry again!\n\n");
-        }
-        else{
-            printf("Valid entry.\n");
-           break; 
-        }
-    }
-}
-
-void ValidateDouble(double * check, double a, double b, char []title){
-    printf("Enter the %s\n", title);
-    while(scanf("%lf%*c", check)==1){
-       
-        if (*check > b|| *check < a) 
-        {
-            printf("Invalid entry.\nTry again!\n\n");
-        }
-        else{
-            printf("Valid entry.\n");
-           break; 
-        }
-    }
-
-}
 int menu(void){
    int choice = 0; 
        printf("%s\n",NAME);
@@ -116,37 +78,48 @@ void add(void){
     int product_number, product_type, product_quantity;  
     double product_cost, product_price; 
     char choice;
-    
+    double price, cost, profit, total_price, total_cost;
+    /*sums */
+    double sum_profit = 0;
+    double sum_price = 0;
+    double sum_cost = 0;
+    int sum_quantity = 0;
+
    do{
        /* User data entry*/
     printf("%s\n\n", NAME);
-    ValidateInt(&product_number,1,9999, "number");
-    ValidateInt(&product_type,1,5, "type");
-    ValidateInt(&product_quantity,1,50, "quantity");
-    ValidateDouble(&product_cost,5.0,900.0,"cost");
-    ValidateDouble(&product_price,6.0,1000.0, "price");
-    void show(product_number, product_type, product_quantity, product_cost, product_price);
-     
-   
+    ValidateInt(&product_number,1,9999, "product number");
+    ValidateInt(&product_type,1,5, "product type");
+    ValidateInt(&product_quantity,1,50, "product quantity");
+    ValidateDouble(&product_cost,5.0,900.0,"product cost");
+    ValidateDouble(&product_price,6.0,1000.0, "product price");
+
     printf("These are this entries calculations.\n");
-
-    double price, cost, profit, total_price, total_cost;
-
+    show(product_number, product_type, product_quantity, product_cost, product_price);
+     
     /*Business logic functions */
     price = Price(product_quantity , product_price);
     cost = Cost(product_cost , product_quantity);
     profit = Profit(price, cost);
 
-    showTotal(price, cost, profit);
+    /*  Total for all entries */
+    /*Finally calculations for business logic */
+    sum_price += price;
+    sum_cost += cost;
+    sum_profit += profit;
+    sum_quantity++;
 
-    printf("Would you like to continue? (type Y/N)\n");
+    showTotal(sum_quantity, price, cost, profit);
+
+    printf("Would you like to add another? (type Y/N)\n");
     scanf(" %c%*c", &choice);
   
-    while(toupper(choice)=='Y');
-    }
+   }while(toupper(choice)=='Y');
+    
 }
 
-void show(product_number, product_type, product_quantity, product_cost, product_price){
+
+void show(int product_number, int product_type, int product_quantity, double product_cost, double product_price){
 
       /* Single product display */
     printf("\nThe product number is ------ %04d\n", product_number);
@@ -156,25 +129,62 @@ void show(product_number, product_type, product_quantity, product_cost, product_
     printf("The price is ------ $%.2lf\n\n", product_price);
 }
 
-void showTotal(price, cost, profit){
+void showTotal(int total, double price, double cost, double profit){
 
+    printf("Totals of all products\n");
     /* Single product business display */
+   
+    printf("Total count of different products -> %d\n", total);
     printf("Total product price ------ $%4.2lf\n", price);
     printf("Total product cost ------ $%4.2lf\n", cost);
     printf("Total product profit ------ $%4.2lf\n", profit);
 }
+double Profit(double tp, double tc){
+    double total;
+    total = tp - tc;
+    return total;
+}
 
-void totalCalc(price, cost, profit){
- /*sums */
-    double sum_profit = 0;
-    double sum_price = 0;
-    double sum_cost = 0;
-    int sum_quantity = 0;
+double Price(int q , double p){
+    double total;
+    total = q * p;
+    return total;
+}
 
-/*  Total for all entries */
-/*Finally calculations for business logic */
-    sum_price += price;
-    sum_cost += cost;
-    sum_profit += profit;
+double Cost(double c, int q){
+    double total;
+    total = c * q;
+    return total;
+}
+
+void ValidateInt(int * check, int a, int b, char title[]){
+    printf("Enter the %s\n", title);
+    while(scanf("%d%*c", check)==1){
+       
+        if (*check > b|| *check < a) 
+        {
+            printf("Invalid entry.\nTry again!\n\n");
+        }
+        else{
+            printf("Valid entry.\n");
+           break; 
+        }
+    }
+}
+
+void ValidateDouble(double * check, double a, double b, char title[]){
+    printf("Enter the %s\n", title);
+    while(scanf("%lf%*c", check)==1){
+       
+        if (*check > b|| *check < a) 
+        {
+            printf("Invalid entry.\nTry again!\n\n");
+        }
+        else{
+            printf("Valid entry.\n");
+           break; 
+        }
+    }
 
 }
+
