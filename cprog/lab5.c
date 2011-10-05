752c9e012a4e961914e877950009843f6590143e
Matthew Clemens <mclemens66+github@gmail.com>
2011-10-05 13:51:19 -0700

    Added validInt and validDouble and validChar
    Need to address the addEntry exit condition.
    Right now it will only except lower case y's because it uses the
    validChar logic, so it wont continue without the single answer. Need a
    variable valid Char function to choose over a range of answers.

--- cprog/lab5.c
+++ cprog/lab5.c
@@ -9,18 +9,19 @@ Date = 10/03/11
 #include <stdbool.h>
 #define NAME "Sierra Sporting Goods"
 
-double Profit(double, double);
-double Price(int, double);
-double Cost(double, int);
-void ValidateInt(int * check, int a , int b, char[]);
-void ValidateDouble(double * check, double a, double b, char[]);
+double profit(double, double);
+double price(int, double);
+double cost(double, int);
+void printValidInt(int * check, int a , int b, char[]);
+void printValidDouble(double * check, double a, double b, char[]);
 
 int menu(void);
-int menuValidate(char  [], int , int );
+int validInt(char  [], int , int);
+char validChar(char [], char);
 void add(void);
 void show(int, int, int, double, double);
 void showTotal(int, double, double, double);
-
+void addEntry(void);
 int main(void)
 {
     
@@ -31,12 +32,13 @@ int main(void)
     choice = menu(); 
     switch(choice)
     {
-        case 1: add();
+        case 1: addEntry();
                 break;
         case 2:
         case 3:
         case 4:
         case 5: more=false;
+                printf("GoodBye\n");
                 break;
         default: printf("GoodBye\n");
                  more =false;
@@ -59,20 +61,7 @@ int menu(void){
         printf("3 = Delete a record\n");
         printf("4 = Change a record\n");
         printf("5 = Quit\n");
-        menuValidate("Choose the number for a menu option",1, 5);
-}
-int menuValidate(char  mesg[], int low, int high){
-    int choice;
-    printf("%s\n",mesg);
-    while(scanf("%d%*c", &choice)==1){
-    if (choice>high || choice <low) {
-        printf("Thats not a choice!\nTry again!\n");
-        }
-    else{
-        printf("Valid Entry\n");
-        return choice;
-        }
-    }
+        validInt("Choose the number for a menu option",1, 5);
 }
 
 void add(void){
@@ -80,38 +69,38 @@ void add(void){
     int product_number, product_type, product_quantity;  
     double product_cost, product_price; 
     char choice;
-    double price, cost, profit, total_price, total_cost;
+    double vprice, vcost, vprofit, total_price, total_cost;
     /*sums */
     double sum_profit = 0;
     double sum_price = 0;
     double sum_cost = 0;
     int sum_quantity = 0;
 
+    printf("%s\n\n", NAME);
    do{
        /* User data entry*/
-    printf("%s\n\n", NAME);
-    ValidateInt(&product_number,1,9999, "product number");
-    ValidateInt(&product_type,1,5, "product type");
-    ValidateInt(&product_quantity,1,50, "product quantity");
-    ValidateDouble(&product_cost,5.0,900.0,"product cost");
-    ValidateDouble(&product_price,6.0,1000.0, "product price");
+    printValidInt(&product_number,1,9999, "product number");
+    printValidInt(&product_type,1,5, "product type");
+    printValidInt(&product_quantity,1,50, "product quantity");
+    printValidDouble(&product_cost,5.0,900.0,"product cost");
+    printValidDouble(&product_price,6.0,1000.0, "product price");
 
-    printf("These are this entries calculations.\n");
     show(product_number, product_type, product_quantity, product_cost, product_price);
-     
-    /*Business logic functions */
-    price = Price(product_quantity , product_price);
-    cost = Cost(product_cost , product_quantity);
-    profit = Profit(price, cost);
+    /* 
+    //Business logic functions 
+    vprice = price(product_quantity , product_price);
+    vcost = cost(product_cost , product_quantity);
+    vprofit = profit(vprice, vcost);
 
-    /*  Total for all entries */
-    /*Finally calculations for business logic */
-    sum_price += price;
-    sum_cost += cost;
-    sum_profit += profit;
+   //   Total for all entries 
+    //Finally calculations for business logic 
+    sum_price += vprice;
+    sum_cost += vcost;
+    sum_profit += vprofit;
     sum_quantity++;
 
-    showTotal(sum_quantity, price, cost, profit);
+    showTotal(sum_quantity, vprice, vcost, vprofit);
+    */
 
     printf("Would you like to add another? (type Y/N)\n");
     scanf(" %c%*c", &choice);
@@ -119,10 +108,32 @@ void add(void){
    }while(toupper(choice)=='Y');
     
 }
+void addEntry(void){
+    int product_number, product_type, product_quantity;  
+    double product_cost, product_price; 
+    char choice;
+ 
+   do{
+       /* User data entry*/
+    printValidInt(&product_number,1,9999, "product number");
+    printValidInt(&product_type,1,5, "product type");
+    printValidInt(&product_quantity,1,50, "product quantity");
+    printValidDouble(&product_cost,5.0,900.0,"product cost");
+    printValidDouble(&product_price,6.0,1000.0, "product price");
+
+    show(product_number, product_type, product_quantity, product_cost, product_price);
+
+    choice=validChar("Would you like to add another? (type Y/N)\n", 'Y');
+   }while(toupper(choice)=='Y');
+}
+    
+
+
 
 
 void show(int product_number, int product_type, int product_quantity, double product_cost, double product_price){
 
+    printf("These are this entries calculations.\n");
       /* Single product display */
     printf("\nThe product number is ------ %04d\n", product_number);
     printf("The product type is ------ %d\n", product_type);
@@ -141,52 +152,95 @@ void showTotal(int total, double price, double cost, double profit){
     printf("Total product cost ------ $%4.2lf\n", cost);
     printf("Total product profit ------ $%4.2lf\n", profit);
 }
-double Profit(double tp, double tc){
+double profit(double tp, double tc){
     double total;
     total = tp - tc;
     return total;
 }
 
-double Price(int q , double p){
+double price(int q , double p){
     double total;
     total = q * p;
     return total;
 }
 
-double Cost(double c, int q){
+double cost(double c, int q){
     double total;
     total = c * q;
     return total;
 }
 
-void ValidateInt(int * check, int a, int b, char title[]){
+void printValidInt(int * check, int a, int b, char title[]){
+    bool input = false;
+    bool correct = false;
     printf("Enter the %s\n", title);
-    while(scanf("%d%*c", check)==1){
+    do{
+    input= scanf("%d%*c", check);
        
-        if (*check > b|| *check < a) 
+        if (!input|| *check > b|| *check < a) 
         {
             printf("Invalid entry.\nTry again!\n\n");
+            getchar();
         }
         else{
-            printf("Valid entry.\n");
-           break; 
+            printf("Valid entry.\n\n");
+            correct = true;
         }
-    }
+    }while(!correct);
 }
 
-void ValidateDouble(double * check, double a, double b, char title[]){
+void printValidDouble(double * check, double a, double b, char title[]){
+    bool input = false;
+    bool correct = false;
     printf("Enter the %s\n", title);
-    while(scanf("%lf%*c", check)==1){
+    do{
+    input = scanf("%lf%*c", check);
        
-        if (*check > b|| *check < a) 
+        if (!input|| *check > b|| *check < a) 
         {
             printf("Invalid entry.\nTry again!\n\n");
+            getchar();
         }
         else{
-            printf("Valid entry.\n");
-           break; 
+            printf("Valid entry.\n\n");
+            correct = true;
         }
-    }
+    }while(!correct);
 
 }
-
+int validInt(char  mesg[], int low, int high){
+    int choice;
+    bool input=false;
+    bool correct = false;
+    printf("%s\n",mesg);
+    do{
+        input =scanf("%d%*c", &choice);
+            if (!input||choice>high || choice <low) {
+                printf("Thats not a choice!\nTry again!\n");
+                getchar();
+            }
+    else{
+        printf("Valid Entry\n\n");
+        correct = true;
+        }
+    }while(!correct);
+    return choice;
+}
+char validChar(char  mesg[], char choice){
+    char pick;
+    bool input = false;
+    bool correct = false;
+    printf("%s\n",mesg);
+    do{
+        input =scanf("%c%*c", &pick);
+            if (!input||toupper(pick)!=choice) {
+        printf("Thats not the right choice!\nTry again!\n");
+        getchar();
+        }
+    else{
+        printf("Valid Entry\n\n");
+        correct = true;
+        }
+    }while(!correct);
+    return pick;
+}
